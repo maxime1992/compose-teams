1657f7ad9293fcc1451bf60248b16ab592b1ded9
Maxime ROBERT <maxime.robert1992@gmail.com>
2017-09-03 18:44:21 +0200

    style: update message when not enough/too much people

--- src/app/need-10-players/need-10-players.component.ts
+++ src/app/need-10-players/need-10-players.component.ts
@@ -5,17 +5,30 @@ import { IPlayer } from 'app/players.interface';
 @Component({
   selector: 'app-need-10-players',
   template: `
-    <span *ngIf="selectedPlayers.length < 10; else tooManyPlayers">
-      Not enough player
-    </span>
+    <div fxFill fxLayout="column" fxLayoutAlign="center center">
+      <div>
+        <span *ngIf="selectedPlayers.length < 10; else tooManyPlayers">
+          Not enough player
+        </span>
 
-    <ng-template #tooManyPlayers>
-      Too many players
-    </ng-template>
+        <ng-template #tooManyPlayers>
+          Too many players
+        </ng-template>
 
-    <span>to generate the possible games</span>
+        <span>to generate the possible games</span>
+      </div>
+
+      <md-icon class="color-primary">mood_bad</md-icon>
+    </div>
   `,
-  styles: [``],
+  styles: [
+    `
+    md-icon {
+      font-size: 45px;
+      margin-top: 20px;
+    }
+  `,
+  ],
 })
 export class Need10PlayersComponent implements OnInit {
   @Input() selectedPlayers: IPlayer[];
